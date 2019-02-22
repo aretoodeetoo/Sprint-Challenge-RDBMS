8bb4fffee6c347ba9a11f3e183ee2701bd1aaae9
croy <roy.christie95@gmail.com>
2019-02-22 11:41:17 -0500

    Added seed file for projects for testing purposes

--- index.js
+++ index.js
@@ -12,6 +12,28 @@ const db = knex('knexConfig');
 const server = express();
 server.use(express.json());
 
+// Get Projects (Testing Purposes)
+server.get('/api/projects', async (req, res) => {
+    try{
+        const projects = await db('projects');
+        res.status(200).json(projects);
+    } catch(error) {
+        res.status(500).json(error);
+    }
+})
+
+// Get Project By ID
+server.get('/api/projects/:id', async (req, res) => {
+    try{
+        const project = await db('projects')
+            .where({ id: req.params.id })
+            .first();
+        res.status(200).json(project);
+    } catch(error) {
+        res.status(500).json(error);
+    }
+});
+
 const port = 3300;
 server.listen(port, function(){
     console.log(`Server listening on port 3300`);
